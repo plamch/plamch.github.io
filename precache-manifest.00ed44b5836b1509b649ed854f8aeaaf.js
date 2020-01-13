996c2f700a3e206499414bc3fa4879ec9044efb7
daniel rodriguez <maieutiquer@gmail.com>
2020-01-13 22:11:37 +0200

    initial commit

---
+++ precache-manifest.00ed44b5836b1509b649ed854f8aeaaf.js
@@ -0,0 +1,75 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa8b2336e3bac57af3b80d46f2d214d5",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "c36b8e09be30bc1e3ddf8260c0e4bbbe",
+    "url": "/img/touch/homescreen512.png"
+  },
+  {
+    "url": "/main.593231174b77dc352ec1.js"
+  },
+  {
+    "revision": "124bd8c7c5394a8dc8117c8f64792ac2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/notification.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/notification.ttf"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/notification.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/notification.eot"
+  },
+  {
+    "revision": "093f0223f6912724bfe7973c8048da8b",
+    "url": "/img/portfolio/humankonsept-logo.png"
+  },
+  {
+    "revision": "593231174b77dc352ec1",
+    "url": "/bundle.css"
+  },
+  {
+    "revision": "90095515d5b9f09974443c7569a8169e",
+    "url": "/img/portfolio/cep-logo-transparent.png"
+  },
+  {
+    "revision": "fd1443bff337d00c0cbb144e1eff2695",
+    "url": "/img/portfolio/cep-logo.gif"
+  },
+  {
+    "revision": "35788cf70835c50598f80370da3d1659",
+    "url": "/img/portfolio/jijdesign-logo.png"
+  },
+  {
+    "revision": "1f24dbf0e4b8858b6f91cbf77afd5685",
+    "url": "/img/portfolio/cep-site.png"
+  },
+  {
+    "revision": "1c5a5e75cfdbd10965c419fb8e44af17",
+    "url": "/img/portfolio/drillnotill-logo.png"
+  },
+  {
+    "revision": "0721a554c1ec80d762b84c665a0f36d9",
+    "url": "/img/portfolio/cep-logo.png"
+  },
+  {
+    "url": "/1.6cb641793d6642a5f913.js"
+  },
+  {
+    "url": "/0.4a89884fc187ba14c01e.js"
+  },
+  {
+    "revision": "a2a4313a9815622805b1830ed175c6f8",
+    "url": "/.htaccess"
+  }
+];
